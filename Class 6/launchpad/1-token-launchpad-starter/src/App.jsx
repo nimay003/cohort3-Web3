3bbf6253c9ccafdca75b06f01102b3c8fe7b9f0e
nimay003 <edunimay003@gmail.com>
2025-05-06 17:27:30 +0530

    Class 6 part 1

---
+++ Class 6/launchpad/1-token-launchpad-starter/src/App.jsx
@@ -0,0 +1,36 @@
+// wallet adapter imports
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
+import './App.css'
+import { TokenLaunchpad } from './components/TokenLaunchpad'
+
+
+function App() {
+  return (
+    <div>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+            <WalletModalProvider>
+              <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: 20
+              }}>
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+              <TokenLaunchpad></TokenLaunchpad>
+            </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  )
+}
+
+
+export default App
